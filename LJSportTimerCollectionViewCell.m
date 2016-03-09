0b465702540f78411f0fa5454f8d6c1be3537433
RyeWhiskeyBourbon <qdfuture0532@vip.qq.com>
2016-03-09 09:30:39 +0800

    storyboard框架
    此为storyboard的大概框架

---
+++ LJSportTimerCollectionViewCell.m
@@ -0,0 +1,17 @@
+//
+//  LJSportTimerCollectionViewCell.m
+//  SunFreeLander
+//
+//  Created by qianfeng on 16/3/9.
+//  Copyright © 2016年 RyeWhiskey. All rights reserved.
+//
+
+#import "LJSportTimerCollectionViewCell.h"
+
+@implementation LJSportTimerCollectionViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+@end
---
+++ LJSportViewController.h
@@ -0,0 +1,14 @@
+//
+//  LJSportViewController.h
+//  SunFreeLander
+//
+//  Created by qianfeng on 16/3/8.
+//  Copyright © 2016年 RyeWhiskey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LJSportViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UICollectionView *LJSportCollectionView;
+
+@end
